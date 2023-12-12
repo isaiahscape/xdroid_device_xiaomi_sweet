344ae35a0b018d7c15007945f712e73710817319
Isaiah <lqlp0011@gmail.com>
2023-12-12 13:03:35 +0800

    kernel: Switch back to old VantomKernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
 echo 'Starting to clone stuffs needed for your device'
 echo 'Cloning Vendor tree [1/2]'
 # Vendor tree
-git clone https://github.com/isaiahscape/vendor_xiaomi_sweet -b xdroid-caf vendor/xiaomi/sweet
+git clone https://github.com/isaiahscape/xdroid_vendor_xiaomi_sweet -b xdroid-caf vendor/xiaomi/sweet
 echo 'Cloning Kernel tree [2/2]' 
 # Kernel tree
-git clone https://github.com/itsshashanksp/kernel_xiaomi_sm6150 -b perf kernel/xiaomi/sm6150
+git clone https://github.com/isaiahscape/xdroid_kernel_xiaomi_sweet -b arrow-11.0 kernel/xiaomi/sm6150
 
 echo 'Done! Now, proceed to lunch.'
 echo 'xdroidCAF Re-piped Edition 1.9.5'
