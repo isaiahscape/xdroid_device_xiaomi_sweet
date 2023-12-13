60b4ca0b6baddd50a3dc5d6ca7641e6f37b9b4f1
Isaiah <lqlp0011@gmail.com>
2023-12-13 14:22:35 +0800

    Directory fix-up

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ echo 'Cloning Vendor tree [1/2]'
 git clone https://github.com/isaiahscape/xdroid_vendor_xiaomi_sweet -b xdroid-caf vendor/xiaomi/sweet
 echo 'Cloning Kernel tree [2/2]' 
 # Kernel tree
-git clone https://github.com/isaiahscape/xdroid_kernel_xiaomi_sweet -b arrow-11.0 kernel/xiaomi/sm6150
+git clone https://github.com/isaiahscape/xdroid_kernel_xiaomi_sweet -b arrow-11.0 kernel/xiaomi/sweet
 
 echo 'Done! Now, proceed to lunch.'
 echo 'xdroidCAF Re-piped Edition 1.9.5'
