42b07e13e02fda0a5f47686ab4ce6201b2eb7a09
Isaiah <lqlp0011@gmail.com>
2023-12-24 20:02:53 +0800

    Removing kernel inside

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,8 @@
 echo 'Starting to clone stuffs needed for your device'
-echo 'Cloning Vendor tree [1/2]'
+echo 'Note: I removed the default vendor here, use @itsshashanksp perf kernel'
+echo 'Cloning Vendor tree [1/1]'
 # Vendor tree
 git clone https://github.com/isaiahscape/xdroid_vendor_xiaomi_sweet -b xdroid-caf vendor/xiaomi/sweet
-echo 'Cloning Kernel tree [2/2]' 
-# Kernel tree
-git clone https://github.com/isaiahscape/xdroid_kernel_xiaomi_sweet -b arrow-11.0 kernel/xiaomi/sweet
 
 echo 'Done! Now, proceed to lunch.'
-echo 'xdroidCAF Re-piped Edition 1.9.5'
+echo 'xdroidCAF Re-piped Edition 1.9.6'
